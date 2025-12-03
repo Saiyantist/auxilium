206670801c7ff87a19d6defa77def7f3b072cc91
Saiyantist <103047887+Saiyantist@users.noreply.github.com>
2025-12-03 10:59:53 +0800

    refactor: Defined types for D.R.Y. and modified API routing
    - Also added first_name and last_name to response data for the frontend

---
+++ frontend/src/types/ticket.ts
@@ -0,0 +1,24 @@
+import type { User } from './user';
+// import type { Category } from './category';
+// import type { Project } from './project';
+
+export interface Ticket {
+  id: number;
+  number: string;
+  subject: string;
+  description: string | null;
+  status: number;
+  priority: number;
+  severity: number;
+  ticket_type: number;
+  category_id: number | null;
+  project_id: number | null;
+  creator_id?: User;
+  assignee_id?: User | null;
+  // creator_id: number;
+  // assignee_id: number | null;
+  due_date: string | null;
+  closed_at: string | null;
+  created_at: string;
+  updated_at: string;
+}
