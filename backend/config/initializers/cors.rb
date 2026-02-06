2a7ff933a9d60600e1fdca82a8df1543d8c7cd07
Saiyantist <103047887+Saiyantist@users.noreply.github.com>
2026-02-06 14:56:47 +0800

    hotfix: Gemfile and Cors
    - Align Ruby version with Render (3.4.6)
    - Frontend url for rails cors origin setting

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -8,7 +8,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     # Where requests come from, the FRONT-END
-    origins "http://localhost:5173"
+    origins ENV.fetch("FRONTEND_URL")
 
     resource "*",
       headers: :any,
